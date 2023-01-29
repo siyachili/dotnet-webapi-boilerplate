fe67fb4aabcc52325aebbb09b0ddd6c24a76699b
Siyabonga Chili <siyabonga.chili@integrove.com>
2023-01-29 08:57:36 +0200

    First commit

---
+++ FSH.Starter/tests/Infrastructure.Test/Caching/DistributedCacheService.cs
@@ -0,0 +1,16 @@
+﻿using FSH.Starter.Infrastructure.Common.Services;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Test.Caching;
+
+public class DistributedCacheService : CacheService<FSH.Starter.Infrastructure.Caching.DistributedCacheService>
+{
+    protected override FSH.Starter.Infrastructure.Caching.DistributedCacheService CreateCacheService() =>
+        new(
+            new MemoryDistributedCache(Options.Create(new MemoryDistributedCacheOptions())),
+            new NewtonSoftService(),
+            NullLogger<FSH.Starter.Infrastructure.Caching.DistributedCacheService>.Instance);
+}
---
+++ FSH.Starter/tests/Infrastructure.Test/Caching/LocalCacheService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Infrastructure.Test.Caching;
+
+public class LocalCacheService : CacheService<FSH.Starter.Infrastructure.Caching.LocalCacheService>
+{
+    protected override FSH.Starter.Infrastructure.Caching.LocalCacheService CreateCacheService() =>
+        new(
+            new MemoryCache(new MemoryCacheOptions()),
+            NullLogger<FSH.Starter.Infrastructure.Caching.LocalCacheService>.Instance);
+}
